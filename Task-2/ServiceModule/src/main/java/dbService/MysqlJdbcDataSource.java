094cf4c06727a51594e125ecdeac7a799bf84c29
KNPNJIJI <51484168+KNPNJIJI@users.noreply.github.com>
2022-11-20 21:33:18 +0300

    Fix Task-2

--- Task-2/ServiceModule/src/main/java/dbService/MysqlJdbcDataSource.java
+++ Task-2/ServiceModule/src/main/java/dbService/MysqlJdbcDataSource.java
@@ -30,21 +30,6 @@ public class MysqlJdbcDataSource {
                 jdbcProperties.getProperty("url"),
                 jdbcProperties.getProperty("username"),
                 jdbcProperties.getProperty("password"));
-
-        connection.createStatement().executeUpdate(
-                "CREATE TABLE if not exists `expenditure` (" +
-                        "`id` INT NOT NULL AUTO_INCREMENT, " +
-                        "`pay_date` DATE NULL, " +
-                        "`payment_receiver` INT NULL, " +
-                        "`amount` DECIMAL NULL, " +
-                        " PRIMARY KEY (`id`)) "
-        );
-        connection.createStatement().executeUpdate(
-                "CREATE TABLE if not exists `receiver` ( " +
-                        "  `id` INT NOT NULL AUTO_INCREMENT, " +
-                        "  `name` VARCHAR(255) NULL, " +
-                        "  PRIMARY KEY (`id`)); "
-        );
         return connection;
     }
 }
